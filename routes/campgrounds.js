4c676473ce417f1cea0bccd16a11dc12e0fef87c
Swapnil Jain <jainswapnil138@gmail.com>
2023-09-05 19:08:51 +0530

    Restructured using Exp Routes & added Flash popups

---
+++ routes/campgrounds.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router(); 
+const catchAsync = require("../utils/catchAsync"); //error function wrapper.
+const ExpressError = require("../utils/ExpressError"); //error class.
+const Campground = require("../models/campground"); //campground schema.
+const { campgroundSchema } = require("../joiSchema"); //joi schemas
+
+//the below function is defined so that it is easier to use 'joi' library.
+//Joi is used for server side validation for campgrounds.
+const validateCampground = (req, res, next) => {
+    //note that campgroundSchema is defined in joiSchema.js file.
+    const { error } = campgroundSchema.validate(req.body); //ensures req.body has validation on server side using joi library.
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(","); //basically 'error.details' is an array which we need to traverse to fetch the correct stuff which we need to use during throw.
+        throw new ExpressError(msg, 400);
+    } else next();
+};
+
+//main camp page
+router.get(
+    "/",
+    catchAsync(async (req, res) => {
+        const campgrounds = await Campground.find({});
+        res.render("campgrounds/index", { campgrounds });
+    })
+);
+
+//form to add a new campground
+router.get("/new", (req, res) => {
+    res.render("campgrounds/new.ejs");
+});
+//where the new campground submits to:
+router.post(
+    "/",
+    validateCampground,
+    catchAsync(async (req, res) => {
+        
+        const newCampground = new Campground(req.body.campground); //making the new campground.
+        await newCampground.save();
+        req.flash("success", "Successfully made a new campground !"); //adding the flash message.
+        res.redirect(`/campgrounds/${newCampground._id}`);
+    })
+);
+
+//A note: The order of the code block having /campgrounds/new and the codeblock having /campgrounds/:id MATTERS.
+//This is cuz otherwise /campgrounds/:id will try to find id='new' if it is declared first.
+
+//getting details of a specific campground.
+router.get(
+    "/:id",
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const campground = await Campground.findById(id).populate("reviews");
+        if (!campground) { //in case campground doesn't exist.
+            req.flash("error", "Campground doesn't exist"); 
+            res.redirect("/campgrounds");
+        }
+        res.render("campgrounds/show.ejs", { campground });
+    })
+);
+
+//editing product, part 1
+router.get(
+    "/:id/edit",
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const campground = await Campground.findById(id);
+        if (!campground) {
+            //in case campground doesn't exist.
+            req.flash("error", "Campground doesn't exist");
+            res.redirect("/campgrounds");
+        }
+        res.render("campgrounds/edit", { campground });
+    })
+);
+
+//part-2: actualy replacing the campground.
+router.put(
+    "/:id",
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const updatedCampground = await Campground.findByIdAndUpdate(
+            id,
+            { ...req.body.campground },
+            {
+                runValidators: true,
+            }
+        ); //remember that runValidators is used to ensure the checks defined in schema are enforced.
+        req.flash("success", "Successfully edited the campground"); //adding the flash message.
+        res.redirect(`/campgrounds/${id}`);
+    })
+);
+
+//deleting the campground and the corresponding reviews.
+router.delete(
+    "/:id",
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const campground = await Campground.findByIdAndDelete(id);
+        req.flash("success", "Campground Deleted Successfully"); //adding the flash message.
+        res.redirect("/campgrounds");
+    })
+);
+
+module.exports = router;
---
+++ routes/reviews.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+//IMP: mergeParams: true enables us to have access to :id parameter, which is otherwise inaccessible in a route.
+const catchAsync = require("../utils/catchAsync"); //error function wrapper.
+const ExpressError = require("../utils/ExpressError"); //error class.
+const Campground = require("../models/campground"); //campground schema.
+const Review = require("../models/review");
+const { reviewSchema } = require("../joiSchema"); //joi schemas
+
+
+//server side validation for reviews using Joi.
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(msg, 400);
+    } else next();
+};
+
+//adding the review functionality.
+router.post(
+    "/",
+    validateReview,
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const newReview = new Review(req.body.review); //make the new review.
+        const campground = await Campground.findById(id); //find the corresponding campground
+        campground.reviews.push(newReview); //push the new review into db.
+        await campground.save();
+        await newReview.save();
+        req.flash("success", "Successfully added review "); //adding the flash message.
+        res.redirect(`/campgrounds/${id}`);
+    })
+);
+//review deleting feature
+router.delete(
+    "/:reviewId",
+    catchAsync(async (req, res) => {
+        let { id, reviewId } = req.params;
+        //the first thing to do is go inside the correct campground and remove the id of the review which we are going to delete.
+        //We use a mongo operator called 'pull' here.
+        await Campground.findByIdAndUpdate(id, {
+            $pull: { reviews: reviewId },
+        });
+        await Review.findByIdAndDelete(reviewId); //delete the actual review.
+        req.flash("success", "Review Deleted Successfully"); //adding the flash message.
+        res.redirect(`/campgrounds/${id}`);
+    })
+);
+
+module.exports = router;
